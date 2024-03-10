11adb4bfd3d934033e84f69179e3e49ce68f7b55
INSIGHT-SURGE <162888791+INSIGHT-SURGE@users.noreply.github.com>
2024-03-10 19:58:17 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -8,7 +8,7 @@ async def start_srb(event, st):
                       buttons=[
                               [Button.inline("SET THUMB.", data="set"),
                                Button.inline("REM THUMB.", data="rem")],
-                              [Button.url("💋 CREATER 💋", url="t.me/INSIGHT_SURGE")]])
+                              [Button.url(" 💋 CREATER 💋", url="t.me/INSIGHT_SURGE")]])
                               
     
 async def vc_menu(event):
@@ -18,5 +18,5 @@ async def vc_menu(event):
                          Button.inline("SOURCE", data="source")],
                         [Button.inline("NOTICE.", data="notice"),
                          Button.inline("Main.", data="help")],
-                        [Button.url("💋 CREATER 💋", url="t.me/INSIGHT_SURGE")]])
+                        [Button.url(" 💋 CREATER 💋", url="t.me/INSIGHT_SURGE")]])
     
